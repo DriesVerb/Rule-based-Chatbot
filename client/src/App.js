310317b3c69b00da2c1277c44faabae575a9e389
DriesVerb <verboven.dries@gmail.com>
2021-05-28 20:04:18 +0200

    setting up scss / redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// components
+import Navbar from "./components/layout/Navbar";
+
+// styling
+import "./App.scss";
+
+//redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = (props) => {
-  return <div>test</div>;
+  return (
+    <Provider store={store}>
+      <Fragment>
+        <div className="container-outside">
+          <Router>
+            <Navbar />
+            <Switch>
+              <div className="container-inside">Hello</div>
+            </Switch>
+          </Router>
+        </div>
+      </Fragment>
+    </Provider>
+  );
 };
 
 App.propTypes = {};
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { Fragment } from "react";
+
+const Navbar = (props) => {
+  return (
+    <Fragment>
+      <div>Navbar</div>
+    </Fragment>
+  );
+};
+
+Navbar.propTypes = {};
+
+export default Navbar;
